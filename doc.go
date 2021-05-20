549dfcd1f440145bb738b240c7c7c8b35de42142
Cameron Newman <cameron@newman.io>
2021-05-20 13:58:02 +1000

    Updated docs

--- doc.go
+++ doc.go
@@ -6,12 +6,13 @@ package pagination
 // Simple example:
 //
 //	page := Page{
-//		ID: "100",
-//		OpenedAtUTC: 21938120938,
-//		Size: 10,
+//		ID: "8fed1b32-3059-4f89-96ec-3af15b3d2bc8",
+//		OpenedAtUTC: 1621480176,
+//		Size: 1,
 //	}
 //
 //	token := page.Encode()
 //
 //	fmt.Println(token)
+//	eyJpZCI6IjhmZWQxYjMyLTMwNTktNGY4OS05NmVjLTNhZjE1YjNkMmJjOCIsIm9wZW5fYXRfdXRjIjoxNjIxNDgwMTc2LCJzaXplIjoxfQ==
 //
